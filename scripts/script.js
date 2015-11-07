f06de629c2afa110a206c7c30088f3434d0aa824
deenaacree <dacree@ufl.edu>
2015-11-07 08:27:14 -0500

    Completed assignment (2).
    Fixed validation error. Still working for a fix for the desktop jQuery
    animation issue. Mobile works fine!

---
+++ scripts/script.js
@@ -0,0 +1,39 @@
+
+$(document).ready(function(){
+  $("#nighttime_sky").click(function(){
+    $(this).animate({
+      width: 1024,
+      height: 683,
+    })
+  });
+$("#ocean_spray").click(function(){
+  $(this).animate({
+    width: 1024,
+    height: 683,
+  })
+});
+$("#san_francisco").click(function(){
+  $(this).animate({
+    width: 1024,
+    height: 575,
+  })
+});
+$("#hamburg_river").click(function(){
+  $(this).animate({
+    width: 1024,
+    height: 676,
+  })
+});
+$("#winter_coffee").click(function(){
+  $(this).animate({
+    width: 1024,
+    height: 778,
+  })
+});
+$("#sunset_lake").click(function(){
+  $(this).animate({
+    width: 1024,
+    height: 683,
+  })
+});
+});
